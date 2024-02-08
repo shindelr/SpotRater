b05dc6c65eeb568e1c435e3b4c101a6f701a9391
shindelr <129821483+shindelr@users.noreply.github.com>
2024-02-08 12:35:40 -0800

    Set up Ag -> home, home -> ag navigation and enabled Agate widget. TODO: - Implement Otter rock widget - Implement home -> or, or -> home navigation

--- src/MainUI.py
+++ src/MainUI.py
@@ -5,8 +5,9 @@ from PyQt5 import QtWidgets, uic
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QWidget, QStackedWidget
 from PyQt5.QtGui import QColor, QPalette
-from InfoPage import Ui_Form
+from sbInfoPage import Ui_Form as sbInfo
 from homeWidget import Ui_Form as HomeScreen
+from agBeachWidg import Ui_Form as agInfo
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -17,16 +18,20 @@ class MainWindow(QtWidgets.QMainWindow):
         self.widg_stack = QStackedWidget()
         self.sb_widg = SBPage()
         self.home_page = HomePage()
+        self.ag_widg = AgPage()
 
         self.setWindowTitle('The Spot Rater')
         self.setupUi()
 
     def setupUi(self):
-        """TODO:"""
+        """Set up initial main window on start up."""
 
         # Widget stack initialization
         self.widg_stack.addWidget(self.sb_widg)
+        self.widg_stack.addWidget(self.ag_widg)
         self.widg_stack.addWidget(self.home_page)
+
+
         # Immediately display homepage
         self.widg_stack.setCurrentWidget(self.home_page)
         self.setCentralWidget(self.widg_stack)
@@ -37,22 +42,34 @@ class MainWindow(QtWidgets.QMainWindow):
 
 
     def nav_logic(self):
-        """TODO:"""
+        """Route all navigation button signals to correct places."""
+        # SB button handling
         self.home_page.sbNavButton.clicked.connect(self.navigate_to_sb)
-        self.sb_widg.Close.clicked.connect(self.close_sb)
+        self.sb_widg.Close.clicked.connect(self.close_aux_widg)
 
+        # Ag button handling
+        self.home_page.AgNavButton.clicked.connect(self.navigate_to_ag)
+        self.ag_widg.Close.clicked.connect(self.close_aux_widg)
     
+
     def navigate_to_sb(self):
         """Set widget stack to show south beach info page."""
         self.widg_stack.setCurrentWidget(self.sb_widg)
         self.setCentralWidget(self.widg_stack) 
     
 
-    def close_sb(self):
+    def close_aux_widg(self):
         """Return to home from south beach info page"""
         self.widg_stack.setCurrentWidget(self.home_page)
         self.setCentralWidget(self.widg_stack)
         
+    
+    def navigate_to_ag(self):
+        """Set widget stack to show agate info page."""
+        self.widg_stack.setCurrentWidget(self.ag_widg)
+        self.setCentralWidget(self.widg_stack)
+
+
 
 class HomePage(QtWidgets.QWidget, HomeScreen):
     def __init__(self):
@@ -66,7 +83,7 @@ class HomePage(QtWidgets.QWidget, HomeScreen):
         self.setPalette(palette)
 
 
-class SBPage(QtWidgets.QWidget, Ui_Form):
+class SBPage(QtWidgets.QWidget, sbInfo):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
@@ -76,6 +93,19 @@ class SBPage(QtWidgets.QWidget, Ui_Form):
         palette = self.palette()
         palette.setColor(QPalette.Window, QColor(232, 243, 247))
         self.setPalette(palette)
+
+
+class AgPage(QtWidgets.QWidget, agInfo):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        # Background Color:
+        self.setAutoFillBackground(True)
+        palette = self.palette()
+        palette.setColor(QPalette.Window, QColor(232, 243, 247))
+        self.setPalette(palette)
+
 
 
 if __name__ == '__main__':
